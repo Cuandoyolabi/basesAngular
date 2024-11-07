67962e094ed0c17c7aeca8026d97ebe3375b194d
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-11-07 11:30:31 -0600

    Se instalo el paquete UUI para los ids

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { CounterModule } from './counter/counter.module';
 import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -3,7 +3,7 @@ import { Character } from '../interfaces/character.interface';
 import { DbzService } from '../services/dbz.service';
 
 @Component({
-  selector: 'app-dbz-main-page',
+  selector: 'dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent  {
